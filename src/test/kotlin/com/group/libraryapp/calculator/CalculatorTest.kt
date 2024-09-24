bdf2368a0a53a112e4f1ba51e13acdf65e547812
lee-dong-gi <oksk327@naver.com>
2024-09-24 22:50:02 +0900

    feat: 1 ~ 4 강 commit  - 1강. 도서관리 애플리케이션 이해하기  - 2강. 테스트 코드란 무엇인가, 그리고 왜 필요한가?!  - 3강. 코틀린 코드 작성 준비하기  - 4강. 사칙연산 계산기에 대해 테스트 코드 작성하기

---
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -0,0 +1,50 @@
+package com.group.libraryapp.calculator
+
+fun main() {
+    val calculatorTest = CalculatorTest()
+    calculatorTest.addTest()
+    calculatorTest.minusTest()
+}
+
+class CalculatorTest {
+
+    fun addTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.add(3)
+
+        // then
+        if (calculator.number != 8) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun minusTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.minus(3)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun multiplyTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.multiply(3)
+
+        // then
+        if (calculator.number != 15) {
+            throw IllegalArgumentException()
+        }
+    }
+
+}
